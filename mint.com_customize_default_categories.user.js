b4e95d14d5c982ac32ff276362e809bdf17b97b5
Stephen Schrauger <github@schrauger.com>
2015-03-04 13:40:59 -0500

    Adds initial meta information

---
+++ mint.com_customize_default_categories.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name Mint.com Customize Default Categories
+// @namespace com.schrauger.mint.js
+// @author Stephen Schrauger
+// @description Hide or rename default built-in mint.com categories
+// @homepage https://github.com/schrauger/mint.com-customize-default-categories
+// @include https://*.mint.com/*
+// @version 2.2.11
+// @require https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js
+// @grant none
+// @downloadURL https://raw.githubusercontent.com/schrauger/mint.com-customize-default-categories/master/mint.com_customize_default_categories.user.js
+// @updateURL   https://raw.githubusercontent.com/schrauger/mint.com-customize-default-categories/master/mint.com_customize_default_categories.user.js
+// ==/UserScript==
+/*jslint browser: true*/
+/*global jQuery*/
