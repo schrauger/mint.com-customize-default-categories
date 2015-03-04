6dc8e4b900cf635a6b2d07c9eb0d60a1318930dd
Stephen Schrauger <github@schrauger.com>
2015-03-04 16:45:38 -0500

    finished decode function initial version

--- mint.com_customize_default_categories.user.js
+++ mint.com_customize_default_categories.user.js
@@ -52,10 +52,8 @@ function get_default_category_list(){
  *                                  ID, name, subcategories and is_hidden members 
  *                                  for both the category and subcategories
  */
-function decode_bit_array(str_bit_array, array_of_eight_categories){
-  // first, remove the first 4 characters (the unique ID plus a space)
-  str_bit_array = str_bit_array.substring(4); // 0-based, meaning start at character 5 (inclusive)
-  
+function decode_bit_array(str_bit_array_array, array_of_all_categories){
+    
   // second, translate any extra characters into their forbidden character 
   // (double quote is forbidden; use a non-bit-array character to encode)
   
@@ -64,49 +62,59 @@ function decode_bit_array(str_bit_array, array_of_eight_categories){
   // use bitwise operators to see if the subcategory minor id (always 1-9) is marked as hidden
   ('a'.charCodeAt(0) & 00001).toString(2)
   
-  array_of_eight_characters.sort(function(a, b){
+  array_of_all_characters.sort(function(a, b){
     return (a.id - b.id); // sort by id, lowest first
   });
+
+  
+  field_count = 0; // 0, 1, or 2; for the 3 unique fields holding the 23 category hidden attributes
   major_count = 0;
-  for (major_category in array_of_eight_categories){
-    if (array_of_eight_categories.hasOwnProperty(major_category)){
-      
-      major_category.minor_categories.sort(function(a, b){
+  str_bit_array = str_bit_array_array(field_count); // 3 custom fields with attributes
+  // remove the first 4 characters (the unique ID plus a space)
+  str_bit_array = str_bit_array.substring(4); // 0-based, meaning start at character 5 (inclusive)
+ 
+  // loop through each major category and its minor categories and mark them as hidden or not
+  for (major_category in array_of_all_categories){
+    if (array_of_all_categories.hasOwnProperty(major_category)){
+      array_of_all_categories.major_category.minor_categories.sort(function(a, b){
         return (a.id - b.id); // sort by id, lowest first
       });
       
       //hasOwnProperty makes sure we aren't looking at inhereted members
       id = major_category.id;
-      bit_character = major_category.charAt(start_padding + major_count);
+      bit_character = major_category.charAt(major_count * 2);
       
       shift_count = 0; // even if minor category 3 doesn't exist, it might in the future, so make a flag based on minor id 1-9 instead of merely positional (which major categories do due to 100 possibilities and not enough space)
       for (minor_category in major_category.minor_categories){
         if (major_category.minor_categories.hasOwnProperty(minor_category)){
-          minor_category.hidden = is_category_hidden(bit_character, shift_count);
+          
+          // if flag is '1', it is hidden
+          array_of_all_categories.major_category.minor_category.hidden = is_category_hidden(bit_character, shift_count);
+          
+          // next flag
           shift_count += 1;
           if (shift_count > 4) {
             // move to the second character and reset the bit shift counter
-            bit_character = major_category.charAt(start_padding + (major_count * 2) + 1);
+            // note: this assumes only two characters used (no more than 9 subcategories, in groups of 5 (so 5 and 4)); 
+            //       this if statement should only ever be run once within each major category loop.
+            bit_character = major_category.charAt((major_count * 2) + 1);
+            shift_count = 0;
           }
         }
       }
-      
-      for (i = 0; i < 4; i++){
-        // first 5 sub-categories
-        
-      }
-      major_count +=1; // each major category gets two characters in the bit array
-      bit_character = 
-      for (i = 0; i < 3; i++) {
-        // next 4 categories, plus the last one for the major category flag
-        
-      }
-      
-      major_count +=1;
-      
     }
-    
+    major_count +=1;
+    if (major_count > 7) {
+      // each of our custom bit arrays can only hold 8 categories' info. reset the counter and move on to the next bit array
+      major_count = 0;
+      field_count += 1;
+      
+      str_bit_array = str_bit_array_array(field_count); // 3 custom fields with attributes
+      // remove the first 4 characters (the unique ID plus a space)
+      str_bit_array = str_bit_array.substring(4); // 0-based, meaning start at character 5 (inclusive)
+    }
   }
+  return array_of_all_categories;
 }
 
 /**
