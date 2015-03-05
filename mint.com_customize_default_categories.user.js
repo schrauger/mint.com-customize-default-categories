e7533e3d5d874e7a74bda19483c3d4925bd41e2b
Stephen Schrauger <stephen@schrauger.com>
2015-03-05 13:57:40 -0500

    encode is working; still need to save to mint

--- mint.com_customize_default_categories.user.js
+++ mint.com_customize_default_categories.user.js
@@ -5,7 +5,7 @@
 // @description Hide or rename default built-in mint.com categories
 // @homepage https://github.com/schrauger/mint.com-customize-default-categories
 // @include https://*.mint.com/*
-// @version 0.0.1
+// @version 0.0.2
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js
 // @grant none
 // @downloadURL https://raw.githubusercontent.com/schrauger/mint.com-customize-default-categories/master/mint.com_customize_default_categories.user.js
@@ -18,7 +18,7 @@ var bit_flags_per_char = 6; // using 01XXXXXX ASCII codes, which allows for 6 fl
 var unique_id_length = 4; // the bit array starts with '#!1 ' or '#!2 ' or '#!3 '
 var categories_per_string = 8; // with 20 characters per string, and 2 characters per category, we can fit 8 (plus the 4-char unique id)
 var characters_per_category = 2; // with 6 flags, this allows for 11 sub categories and 1 major category
-
+var number_of_bit_arrays = 3; // use 3 arrays to store all preferences.
 
 // Need to define all categories and subcategories, along with their ID. Create this list dynamically.
 function get_default_category_list() {
@@ -89,7 +89,6 @@ function decode_bit_array(str_bit_array_array, array_of_all_categories) {
     for (var category_major_count = 0, category_major_length = array_of_all_categories.length; category_major_count < category_major_length; category_major_count++) {
         var category_major = array_of_all_categories[category_major_count];
 
-        console.log('inside loop:' + category_major.name);
         array_of_all_categories[category_major_count].categories_minor.sort(function (a, b) {
             return (a.id - b.id); // sort by id, lowest first
         });
@@ -102,13 +101,13 @@ function decode_bit_array(str_bit_array_array, array_of_all_categories) {
             var minor_id = category_major.categories_minor[category_minor_count].id.slice(-2); // the last two digits are the minor category; the first two are always the same as the parent major category
             // if flag is '1', it is hidden
             array_of_all_categories[category_major_count].categories_minor[category_minor_count].is_hidden = is_category_hidden(bit_characters, minor_id);
-            console.log(array_of_all_categories[category_major_count].categories_minor[category_minor_count].name + ' is hidden: '
-                            + array_of_all_categories[category_major_count].categories_minor[category_minor_count].is_hidden);
+            //console.debug(array_of_all_categories[category_major_count].categories_minor[category_minor_count].name + ' is hidden: '
+            //                + array_of_all_categories[category_major_count].categories_minor[category_minor_count].is_hidden);
         }
         var last_flag = characters_per_category * bit_flags_per_char; // last flag is used for major category, instead of subcategory #12 (2 * 6);
         array_of_all_categories[category_major_count].is_hidden = is_category_hidden(bit_characters, last_flag);
-        console.log(array_of_all_categories[category_major_count].name + ' is hidden: '
-                            + array_of_all_categories[category_major_count].is_hidden);
+        //console.debug(array_of_all_categories[category_major_count].name + ' is hidden: '
+        //                    + array_of_all_categories[category_major_count].is_hidden);
         bit_string_category_count += 1;
         if (bit_string_category_count > (categories_per_string - 1)) {
             // each of our custom bit arrays can only hold 8 categories' info. reset the counter and move on to the next bit array
@@ -135,6 +134,17 @@ function is_category_hidden(ascii_characters, minor_id) {
     return is_hidden;
 }
 
+function encode_category_hidden(ascii_characters, minor_id, is_hidden){
+    var bit_shift_count = ((minor_id - 1) % bit_flags_per_char); // category 1 is stored in last bit flag; cat 2 in the second to last. cat 7 stored in last flag
+    var bit_to_use = (Math.floor((minor_id - 1) / bit_flags_per_char)); // 1-6 (0-5) in first bit. 7-12 (6-11) stored in second. etc. 7/6 floored is 1.
+    var mask = ((is_hidden << bit_shift_count)); // move the mask to the proper location
+
+    var new_char = String.fromCharCode(ascii_characters[bit_to_use].charCodeAt(0) | mask);
+    ascii_characters = ascii_characters.replaceAt(bit_to_use, new_char); // replace with new character
+    //console.debug(ascii_characters);
+    return ascii_characters;
+}
+
 /**
  * Replaces the substituted characters with the 'illegal' characters.
  * This way, the script can use bitwise operations in a logical manner.
@@ -152,12 +162,6 @@ function translate_to_mint(string_with_illegal_characters) {
     return str_return;
 }
 
-function init() {
-    var str_bit_array_array = extract_mint_array();
-    var default_categories = get_default_category_list();
-    decode_bit_array(str_bit_array_array, default_categories);
-}
-
 /**
  * Extracts the bit arrays from the saved custom category input box and puts all 3 into a string array.
  * Also hides the three fields, since the user probably shouldn't mess with them directly (and they look weird).
@@ -173,4 +177,84 @@ function extract_mint_array() {
 
 }
 
-init();
+function encode_bit_array(array_of_all_categories){
+    // loop through each category, create an ASCII character, and replace illegal characters
+
+    array_of_all_categories.sort(function (a, b) {
+        return (a.id - b.id); // sort by id, lowest first
+    });
+
+    var field_count = 0; // 0, 1, or 2; for the 3 unique fields holding the 23 category hidden attributes
+    var bit_string_category_count = 0; // only 8 categories per string. once this goes past 7, reset and use next string.
+    var str_bit_array_array = [];
+
+    // loop through each major category and its minor categories and mark them as hidden or not
+    str_bit_array_array[field_count] = new Array(characters_per_category * categories_per_string + 1).join( '@' );
+    for (var category_major_count = 0, category_major_length = array_of_all_categories.length; category_major_count < category_major_length; category_major_count++) {
+
+        var category_major = array_of_all_categories[category_major_count];
+
+        array_of_all_categories[category_major_count].categories_minor.sort(function (a, b) {
+            return (a.id - b.id); // sort by id, lowest first
+        });
+
+
+        var bit_characters = new Array(characters_per_category + 1).join( '@' ); // the @ character is 01000000, so all flags (last 6) start 'off'.
+        for (var category_minor_count = 0, category_minor_length = category_major.categories_minor.length; category_minor_count < category_minor_length; category_minor_count++) {
+
+            var minor_id = category_major.categories_minor[category_minor_count].id.slice(-2); // the last two digits are the minor category; the first two are always the same as the parent major category
+            // if flag is '1', it is hidden
+
+            bit_characters = encode_category_hidden(bit_characters, minor_id, array_of_all_categories[category_major_count].categories_minor[category_minor_count].is_hidden);
+            str_bit_array_array[field_count] = str_bit_array_array[field_count].replaceAt(bit_string_category_count * characters_per_category, bit_characters);
+        }
+        var last_flag = characters_per_category * bit_flags_per_char; // last flag is used for major category, instead of subcategory #12 (2 * 6);
+        bit_characters = encode_category_hidden(bit_characters, last_flag, array_of_all_categories[category_major_count].is_hidden);
+
+        str_bit_array_array[field_count] = str_bit_array_array[field_count].replaceAt(bit_string_category_count * characters_per_category, bit_characters);
+
+        bit_string_category_count += 1;
+        if (bit_string_category_count > (categories_per_string - 1)) {
+            // each of our custom bit arrays can only hold 8 categories' info. reset the counter and move on to the next bit array
+            bit_string_category_count = 0;
+            field_count += 1;
+            str_bit_array_array[field_count] = new Array(characters_per_category * categories_per_string + 1).join( '@' );
+        }
+    }
+    // now that all 3 bit arrays are creates, tack on the unique id to the string
+    for (var i=0; i < number_of_bit_arrays; i++){
+        str_bit_array_array[i] = '#!' + (i + 1) + ' ' + str_bit_array_array[i];
+        console.log(str_bit_array_array[i]);
+    }
+    return str_bit_array_array;
+}
+
+/**
+ * Loads the categories and hides the user specified ones.
+ */
+function init() {
+    var str_bit_array_array = extract_mint_array();
+    var default_categories = get_default_category_list();
+    decode_bit_array(str_bit_array_array, default_categories);
+}
+
+/**
+ * Saves the preferences
+ */
+function save() {
+    var default_categories = get_default_category_list();
+
+    var bit_array_array = encode_bit_array(default_categories);
+}
+
+/**
+ * Allows immutible strings to have character(s) replaced
+ * @param index
+ * @param character
+ * @returns {string}
+ */
+String.prototype.replaceAt=function(index, character) {
+    return this.substr(0, index) + character + this.substr(index+character.length);
+}
+
+save();
